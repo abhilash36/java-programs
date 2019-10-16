06a554a6b149d4d660d966f90443d9025cee1e09
Admin <Admin@DESKTOP-4VSMM3J>
2019-10-16 11:53:08 +0530

    added m2 method

--- JPP4/src/arrays/HelloWorld.java
+++ JPP4/src/arrays/HelloWorld.java
@@ -1,7 +1,11 @@
 package arrays;
 
-public class HelloWorld {
-
+public class HelloWorld 
+{
+ public void m2()
+ {
+	 
+ }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
